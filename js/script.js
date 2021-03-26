afbc73c77e0d9d49313b80a696ed44c87483b6b4
konevartur <konev.artur777@gmail.com>
2021-03-26 17:39:11 +0300

    pagination 3

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 const $mainRight = document.getElementById('main-right');
+const $cardsWrapper = document.getElementById('cards-wrapper');
 const $cardTpl = document.getElementById('card-frends-template');
 
 function request(method, url) {
@@ -80,15 +81,25 @@ document.getElementById('main-right').addEventListener('click', event => {
 }) */
 
 
-/* const clickMeBtn = document.getElementById('click-me');
-const pageNumber = document.getElementById('page-number');
-clickMeBtn.addEventListener('click', makeRequest);
+// pagination 
+let pagination = document.querySelector('#pagin');
+// кол-во страниц
+let totalPages = 8;
+
+for (let i = 1; i <= totalPages; i++) {
+	let li = document.createElement('li');
+	li.classList.add('pagin-li')
+	li.innerHTML = i;
+	pagination.appendChild(li);
+}
 
 function makeRequest() {
-	request('GET', `https://reqres.in/api/users?page=${pageNumber.value}&per_page=12`)
+	request('GET', `https://reqres.in/api/users?page=1&per_page=3`)
+
 		.then(function (response) {
 			const cardsArray = response.data;
-			console.log(response.data.length);
+
+			$cardsWrapper.innerHTML = '';
 
 			cardsArray.forEach((user) => {
 
@@ -103,130 +114,21 @@ function makeRequest() {
 				$userName.textContent = `${user.first_name}`;
 				$userAva.setAttribute('src', user.avatar);
 
-				$mainRight.append($card);
+				$cardsWrapper.append($card);
 			});
 		});
 
-	document.getElementById('main-right').addEventListener('click', event => {
+	$mainRight.addEventListener('click', event => {
 
-		const deleteUser = event.target;
-
-		if (deleteUser.classList.contains('btn-delete-user')) {
-			const deleteCardUser = deleteUser.closest('.card-friends');
+		if (event.target.classList.contains('btn-delete-user')) {
+			const deleteCardUser = event.target.closest('.card-friends');
 			deleteCardUser.remove();
 		};
+
+		if (event.target.classList.contains('pagin-li')) {
+			console.log(event.target.textContent);
+		}
 	});
 };
 
 makeRequest();
- */
-
-/* const clickMeBtn = document.getElementById('click-me');
-const pageNumber = document.getElementById('page-number');
-clickMeBtn.addEventListener('click', makeRequest); */
-let notesOnPage = 3;
-function makeRequest(page) {
-	request('GET', `https://reqres.in/api/users?page=${page}&per_page=${notesOnPage}`)
-		.then(function (response) {
-
-			const cardsArray = response.data;
-
-			cardsArray.forEach((user) => {
-
-				const $card = $cardTpl.cloneNode(true);
-
-				$card.removeAttribute('id');
-				$card.removeAttribute('style');
-
-				const $userName = $card.querySelector('.js-user-name');
-				const $userAva = $card.querySelector('.js-user-ava');
-
-				$userName.textContent = `${user.first_name}`;
-				$userAva.setAttribute('src', user.avatar);
-				
-				$mainRight.append($card);
-			});
-		});
-
-	document.getElementById('main-right').addEventListener('click', event => {
-
-		const deleteUser = event.target;
-		if (deleteUser.classList.contains('btn-delete-user')) {
-			const deleteCardUser = deleteUser.closest('.card-friends');
-			deleteCardUser.remove();
-		};
-	});
-};
-
-//pagination 
-//=================================//
-// selecting required element
-let totalPages;
-request('GET', `https://reqres.in/api/users`)
-.then(function (response){
-	totalPages = Math.ceil(response.total / notesOnPage)
-})
-const element = document.querySelector(".pagin ul");
-let page = 1;
-
-//calling function with passing parameters and adding inside element which is ul tag
-element.innerHTML = createPagination(totalPages, page);
-function createPagination(totalPages, page) {
-	let liTag = '';
-	let active;
-	let beforePage = page - 1;
-	let afterPage = page + 1;
-	makeRequest(page)
-	if (page > 1) { //show the next button if the page value is greater than 1
-		liTag += `<li class="btn prev" onclick="createPagination(totalPages, ${page - 1})"><span><i class="fa fa-arrow-circle-o-left" aria-hidden="true"></i></span></li>`;
-	}
-
-	if (page > 2) { //if page value is less than 2 then add 1 after the previous button
-		liTag += `<li class="first numb" onclick="createPagination(totalPages, 1)"><span>1</span></li>`;
-		if (page > 3) { //if page value is greater than 3 then add this (...) after the first li or page
-			liTag += `<li class="dots"><span>...</span></li>`;
-		}
-	}
-
-	// how many pages or li show before the current li
-	if (page == totalPages) {
-		beforePage = beforePage - 2;
-	} else if (page == totalPages - 1) {
-		beforePage = beforePage - 1;
-	}
-	// how many pages or li show after the current li
-	if (page == 1) {
-		afterPage = afterPage + 2;
-	} else if (page == 2) {
-		afterPage = afterPage + 1;
-	}
-
-	for (var plength = beforePage; plength <= afterPage; plength++) {
-		if (plength > totalPages) { //if plength is greater than totalPage length then continue
-			continue;
-		}
-		if (plength == 0) { //if plength is 0 than add +1 in plength value
-			plength = plength + 1;
-		}
-		if (page == plength) { //if page is equal to plength than assign active string in the active variable
-			active = "active";
-		} else { //else leave empty to the active variable
-			active = "";
-		}
-		liTag += `<li class="numb ${active}" onclick="createPagination(totalPages, ${plength})"><span>${plength}</span></li>`;
-	}
-
-	if (page < totalPages - 1) { //if page value is less than totalPage value by -1 then show the last li or page
-		if (page < totalPages - 2) { //if page value is less than totalPage value by -2 then add this (...) before the last li or page
-			liTag += `<li class="dots"><span>...</span></li>`;
-		}
-		liTag += `<li class="last numb" onclick="createPagination(totalPages, ${totalPages})"><span>${totalPages}</span></li>`;
-	}
-
-	if (page < totalPages) { //show the next button if the page value is less than totalPage(20)
-		liTag += `<li class="btn next" onclick="createPagination(totalPages, ${page + 1})"><span><i class="fa fa-arrow-circle-o-right" aria-hidden="true"></i></span></li>`;
-	}
-	element.innerHTML = liTag; //add li tag inside ul tag
-	return liTag; //reurn the li tag
-}
-
