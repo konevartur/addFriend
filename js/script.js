f7e7231d1e0ae2f4fcb846731795c74086260d2f
konevartur <konev.artur777@gmail.com>
2021-04-02 23:26:47 +0300

    modal create friend

--- js/script.js
+++ js/script.js
@@ -1,18 +1,21 @@
+// получение элементов из DOM
 const $mainRight = document.getElementById('main-right');
 const $cardsWrapper = document.getElementById('cards-wrapper');
 const $cardTpl = document.getElementById('card-frends-template');
 
-const $pagination = document.querySelector('#pagin');
-const $loader = document.querySelector('#loader');
+const $preloader = document.querySelector('#preloader');
 
+const $jsBtnInfo = document.querySelector('.js-btn-info');
 const $modal = document.getElementById('mymodal');
 const $closeModal = document.querySelector('.close-mymodal');
 
-
+// флаги
 let preventRequesting = false;
 let stopRequest = true;
+let preventRequestInModal = false;
 let pageNumber = 1;
 
+// HTTP-запрос к серверу без перезагрузки страницы
 function request(method, url) {
 	return new Promise((resolve, reject) => {
 		const xhr = new XMLHttpRequest();
@@ -28,21 +31,24 @@ function request(method, url) {
 	});
 }
 
+// запрос
 function makeRequest() {
 	request('GET', `https://reqres.in/api/users?page=${pageNumber}&per_page=3`)
 		.then(function (response) {
 			const cardsArray = response.data;
-			console.log(response.data);
+
+			// условие для остановки запросов
 			if (cardsArray.length === 0) {
 				stopRequest = false;
 			}
+			// скрываю лоадер
+			$preloader.classList.add('hide');
 
-			$loader.classList.add('hide');
-
+			// предотвращаю запрос
 			preventRequesting = false;
 
+			// перебираю массив и отрисовываю карточку
 			cardsArray.forEach((user) => {
-
 				const $card = $cardTpl.cloneNode(true);
 
 				$card.removeAttribute('id');
@@ -50,32 +56,55 @@ function makeRequest() {
 
 				const $userName = $card.querySelector('.js-user-name');
 				const $userAva = $card.querySelector('.js-user-ava');
-				const $userInfo = $card.querySelector('.js-btn-info');
 
 				$userName.textContent = `${user.first_name}`;
 				$userAva.setAttribute('src', user.avatar);
 
-				$userInfo.addEventListener('click', event => {
-					if (event.target.classList.contains('js-btn-info')) {
-						const $userLastName = document.querySelector('.js-last-name');
-						const $userEmail = document.querySelector('.js-email');
-						$userLastName.textContent = `${user.last_name}`;
-						$userEmail.textContent = `${user.email}`;
-					}
-				})
-
+				$card.setAttribute('data-user_id', user.id);
 				$cardsWrapper.append($card);
+
 			});
 		});
 }
-
+// вызов функции
 makeRequest();
 
+
 // удалить карточку через делегирование
+// открыть модальное окно через делегирование
 $mainRight.addEventListener('click', event => {
 	if (event.target.classList.contains('btn-delete-user')) {
 		const deleteCardUser = event.target.closest('.card-friends');
 		deleteCardUser.remove();
+	}
+
+	// добавление данных в модальное окно
+	if (!preventRequestInModal) {
+		if (event.target.classList.contains('js-btn-info')) {
+
+			const $userCard = event.target.closest('.card-friends');
+			const id = $userCard.getAttribute('data-user_id');
+
+			preventRequestInModal = true;
+
+			request('GET', `https://reqres.in/api/users/${id}`)
+				.then(response => {
+					
+					preventRequestInModal = false;
+
+					const user = response.data;
+
+					const $userName = document.querySelector('#js-user-name');
+					const $userLastName = document.querySelector('.js-last-name');
+					const $userEmail = document.querySelector('.js-email');
+
+					$userName.textContent = `${user.first_name}`;
+					$userLastName.textContent = `${user.last_name}`;
+					$userEmail.textContent = `${user.email}`;
+					
+					mymodal.style.display = 'block';
+				});
+		}
 	}
 });
 
@@ -85,16 +114,9 @@ window.addEventListener('scroll', () => {
 		if (!preventRequesting && (Math.ceil(window.pageYOffset + innerHeight) + 150) > document.body.clientHeight) {
 			preventRequesting = true;
 			pageNumber++;
-			$loader.classList.remove('hide');
+			$preloader.classList.remove('hide');
 			makeRequest();
 		}
-	}
-});
-
-// открыть модальное окно через делегирование
-$mainRight.addEventListener('click', event => {
-	if (event.target.classList.contains('js-btn-info')) {
-		mymodal.style.display = 'block';
 	}
 });
 
@@ -111,3 +133,23 @@ window.addEventListener('click', event => {
 });
 
 
+// модальное окно по созданию записи о друге
+const $btnCreateFriend = document.getElementById('btn-create-friend');
+const $modalCreateFriend = document.getElementById('modal_cf');
+const $closeModalCreateFriend = document.querySelector('.close-modal-cf');
+
+$btnCreateFriend.addEventListener('click', () => {
+	modal_cf.style.display = 'block';
+});
+
+// закрываю модальное окно на крест
+$closeModalCreateFriend.addEventListener('click', () => {
+	modal_cf.style.display = 'none';
+});
+
+// закрываю модальное по нажатию на пустое место
+window.addEventListener('click', event => {
+	if (event.target == $modalCreateFriend) {
+		modal_cf.style.display = 'none';
+	}
+});
